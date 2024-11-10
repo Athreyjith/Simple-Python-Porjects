864c425001cb736dda183efca87f414f6b6222a5
athreyjith <athreyjith46@gmail.com>
2024-11-10 22:37:31 +0530

    added madlibs-game

---
+++ madlibs.py
@@ -0,0 +1,16 @@
+
+
+adjective1 = input(" enter the adjective1")
+verb1 =input(" enter a action")
+noun = input ("enter the name,animal,place or thing")
+adjective = input("enter a description")
+noun1 = input ("enter the name,animal,place or thing")
+adjective2 = input("enter a description")
+verb2 =input(" enter a action")
+adjective3 = input("enter a description")
+
+print(f" {noun} was {adjective1} ")
+print(f" {noun1} came near me with a {adjective2} look")
+print(f" he was {adjective } on me ,That time police came near me by seeing police he started {verb1}")
+print(f"i was {verb2} by seeing him ")
+print(f"it made me {adjective3}")
